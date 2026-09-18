7625d17a959c9076ff803835b534758a16cbdb38
Dario Morales A. <soulmates0127@gmail.com>
2026-09-18 18:06:35 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mi-portafolio/',
+  base: '/Portafolio_Final/',
   plugins: [
     react(),
     tailwindcss(),
